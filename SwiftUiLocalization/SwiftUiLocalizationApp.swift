3db70393d256762122b4398136ac23d7f0c7297a
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-02 14:05:36 +0100

    Initial Commit

---
+++ SwiftUiLocalization/SwiftUiLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLocalizationApp.swift
+//  SwiftUiLocalization
+//
+//  Created by Godwin IE on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
